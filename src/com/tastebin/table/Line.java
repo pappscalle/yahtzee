0056acc142085a4b2e42ba5510d726182e8491cb
Calle <carl.frenn@gmail.com>
2020-07-22 01:34:29 +0200

    replaced class with built-in String function

--- src/com/tastebin/table/Line.java
+++ src/com/tastebin/table/Line.java
@@ -20,11 +20,7 @@ public class Line {
     
     @Override
     public String toString() {
-        if (widths.isEmpty()) {
-            return "";
-        } else {
-            return widths.stream().map(w -> new RepeatedString("-", w).toString()).collect(Collectors.joining("+"));
-        }
+        return widths.stream().map(w -> "-".repeat(w)).collect(Collectors.joining("+"));
     }
     
 }
